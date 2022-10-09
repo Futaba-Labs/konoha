19d62b3343a09a86e37960ef97ea5d7e41a08f34
adachi_440 <tomoki1999314@gmail.com>
2022-10-09 08:28:26 -0500

    modify

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 import ConnectWallet from "./ConnextWallet";
-import { Navbar, Text, Button, Link} from "@nextui-org/react";
+import { Navbar, Text, Button, Link } from "@nextui-org/react";
 
 const NavBar = () => {
   return (
     <Navbar isBordered>
       <Navbar.Brand>
         <Link href="/">
-        <Text b color="black" hideIn="xs">
-          Futaba
-        </Text>
+          <Text b color="black" hideIn="xs">
+            Konoha
+          </Text>
         </Link>
       </Navbar.Brand>
       <Navbar.Content>
-
         <ConnectWallet />
-        <Navbar.Link href="/about">
-          About
-        </Navbar.Link>
+        <Navbar.Link href="/about">About</Navbar.Link>
       </Navbar.Content>
     </Navbar>
   );
